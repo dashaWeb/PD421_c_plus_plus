5b00797c234dba74714a4d77cdbdb7a4dd443ccf
konopelko_d <konopelko_d@itstep.academy>
2024-09-13 15:21:47 +0300

    array sorted helper

--- 05_array sorted/05_array sorted.cpp
+++ 05_array sorted/05_array sorted.cpp
@@ -1,20 +1,31 @@
-// 05_array sorted.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <ctime>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    srand(time(0));
+    const int length = 10;
+    int arr[length]{};
+
+    // fill unique array
+    for (size_t i = 0; i < length;)
+    {
+        int number = rand() % 10 + 1;
+        bool flag = true;
+        for (size_t j = 0; j < i; j++)
+        {
+            if (arr[j] == number) {
+                flag = false;
+                break;
+            }
+        }
+        if (flag)
+        {
+            arr[i] = number;
+            i++;
+        }
+    }
+
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
